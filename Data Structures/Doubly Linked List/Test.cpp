e2836f60a17a54779dce260bcf5cd6af4a8a5822
Piero Violeta Estrella <45082124+PieroVioleta@users.noreply.github.com>
2019-10-09 20:57:41 -0500

    Program for testing DoublyLinkedList DS

---
+++ Data Structures/Doubly Linked List/Test.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include "DoublyList.h"
+using namespace std;
+int main() {
+    D_list lista1;
+    int opcion = -1;
+    while(opcion != 0) {
+        cout << "\nMENU:\n1.- Imprimir\n2.- Insertar inicio\n3.- Buscar\n4.- Insertar final\n5.- Eliminar\n0.- Salir\n";
+        cout << "Ingrese opcion: ";
+        cin >> opcion;
+        cout << endl;
+        if(opcion == 1) {
+            lista1.imprimir();
+        } else if(opcion == 2) {
+            int x;
+            cout << "Ingrese el dato que quiera insertar: ";
+            cin >> x;
+            lista1.insertar_inicio(x);
+        } else if(opcion == 3){
+            int x;
+            cout << "Ingrese el numero que desee buscar: ";
+            cin >> x;
+            if(lista1.buscar(x) == NULL) cout << "No se encuentra en la lista\n";
+            else cout << "Si se encuentra\n";
+        }else if(opcion == 4) {
+            int x;
+            cout << "Ingrese el dato que quiera insertar: ";
+            cin >> x;
+            lista1.insertar_final(x);
+        } else if(opcion == 5) {
+            int x;
+            cout << "Ingrese el numero que desea eliminar: ";
+            cin >> x;
+            lista1.eliminar(x);
+        }
+    }
+    return 0;
+}
